01eb9e0acc187d60fa3147f88fe9332b0adb3272
priya-gupta18 <pg3367770@gmail.com>
2026-08-20 11:13:58 +0530

    JS

---
+++ Javascript/loops.js
@@ -0,0 +1,40 @@
+//for loop
+// const arr=[1,10,5,8,6];
+// for(let i=0;i<arr.length;i++){
+//    // console.log(arr[i]);
+//    console.log(`are elements are:${arr[i]}`);
+// }
+
+//while loop
+// const arr=[10 , 20, 30,40,50];
+// let i=0;
+// while(i<arr.length){
+//     console.log(arr[i]);
+//     i++;
+// }
+
+//do-while
+//const arr=[10,20,30,40,50]
+
+//for of
+// const arr=[1,2,3,4,5,6,7,8];
+// for(const element of arr){
+//     console.log(element);
+// }
+
+//for in 
+// const arr=[10,20,30,40,50];
+// for(const element in arr){
+//     console.log(arr[element]);
+// }
+
+//string
+const names=['priya','seema','richa','nidhi']
+for(const key of names){
+    console.log(key);
+}
+
+const scores=[30,40,50,60]
+for(const key in scores){
+    console.log(key);
+}
